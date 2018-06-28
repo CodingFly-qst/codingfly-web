003c4e355da903f7c08ccd097131fdb09cef4c83
llxxddlxd <llxxdd_lxd@126.com>
2018-06-28 17:34:59 +0800

    0628 添加redis临时配置

--- application/extra/admin.php
+++ application/extra/admin.php
@@ -10,7 +10,11 @@ return [
         'namespace' => 'admin',//后台命名空间及表前缀
         'access_expires'=>1800,
         'remember_expires'=>864000,/*10天*/
-       
+        /*'redis'=>[
+            'host'      =>  '192.168.100.6' ,
+            'port'      =>  6379 ,
+            'database'  =>  15
+        ]*/
     ],
     'page_size'=>10
 ];
