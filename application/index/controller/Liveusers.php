14ac83b936b5204dcc3feb23f93683316ae57883
llxxddlxd <llxxdd_lxd@126.com>
2018-07-04 15:10:08 +0800

    添加个人中控制器

---
+++ application/index/controller/Liveusers.php
@@ -0,0 +1,77 @@
+<?php
+namespace app\index\controller;
+
+use app\index\base\ControllerBase;
+
+class Liveusers extends ControllerBase
+{
+    public function index()
+    {
+        return $this->view->fetch();
+    }
+    public function myliveRoom()
+    {
+        return $this->view->fetch();
+    }
+    public function bindTel()
+    {
+        return $this->view->fetch();
+    }
+    public function inviteBindTel()
+    {
+        return $this->view->fetch();
+    }
+    public function userInfo()
+    {
+        return $this->view->fetch();
+    }
+    public function userInfoEdit()
+    {
+        return $this->view->fetch();
+    }
+    public function myAccount()
+    {
+        return $this->view->fetch();
+    }
+    public function myCoin()
+    {
+        return $this->view->fetch();
+    }
+    public function myCoinExplain()
+    {
+        return $this->view->fetch();
+    }
+    public function myCoinAccount()
+    {
+        return $this->view->fetch();
+    }
+    public function myCoinRanking()
+    {
+        return $this->view->fetch();
+    }
+    public function myInvite()
+    {
+        return $this->view->fetch();
+    }
+    public function myAccountDetail()
+    {
+        return $this->view->fetch();
+    }
+    public function myConcern()
+    {
+        return $this->view->fetch();
+    }
+    public function infoSuccess()
+    {
+        return $this->view->fetch();
+    }
+    public function userInfoDetail()
+    {
+        return $this->view->fetch();
+    }
+    public function myNotice()
+    {
+        return $this->view->fetch();
+    }
+    
+}
